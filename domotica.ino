3f96bd84e0675d484597c7799a3bf8f15b501f3c
Fablab Imperia <fablabimperia@gmail.com>
2017-05-13 14:34:49 +0200

    modificata interfaccia grafica e incluse direttamente librerie RemoteXY per semplificare versionamento

--- domotica.ino
+++ domotica.ino
@@ -1,5 +1,5 @@
 /*
-   -- New project --
+   -- Domotica --
    
    This source code of graphical user interface 
    has been generated automatically by RemoteXY editor.
@@ -23,7 +23,10 @@
 #define REMOTEXY_MODE__SOFTSERIAL
 #include <SoftwareSerial.h>
 
-#include <RemoteXY.h>
+
+#include "RemoteXY.h"
+#include "DHT.h"
+#include "LiquidCrystal_I2C.h"
 
 // RemoteXY connection settings 
 #define REMOTEXY_SERIAL_RX 2
@@ -34,27 +37,36 @@
 // RemoteXY configurate  
 #pragma pack(push, 1)
 uint8_t RemoteXY_CONF[] =
-  { 255,6,0,0,0,92,0,6,5,0,
-  2,0,7,36,18,8,2,79,78,0,
-  79,70,70,0,2,0,7,48,18,8,
-  2,79,78,0,79,70,70,0,2,0,
-  7,6,18,8,2,79,78,0,79,70,
-  70,0,3,3,79,13,8,22,2,1,
-  0,76,48,12,12,1,83,84,79,80,
-  0,2,0,47,6,18,8,2,79,78,
-  0,79,70,70,0,130,1,1,46,97,
-  16,4,130,1,67,1,30,44,9 };
+  { 255,6,0,0,0,174,0,6,8,1,
+  2,0,44,5,16,8,2,79,78,0,
+  79,70,70,0,4,0,49,56,8,28,
+  2,2,0,44,18,17,8,2,79,78,
+  0,79,70,70,0,2,0,44,32,17,
+  8,2,79,78,0,79,70,70,0,2,
+  0,44,45,17,8,2,79,78,0,79,
+  70,70,0,1,0,12,79,17,17,1,
+  71,101,110,101,114,97,108,101,0,129,
+  0,6,7,29,5,5,76,117,99,105,
+  32,73,110,116,101,114,110,101,0,129,
+  0,5,19,31,5,4,76,117,99,105,
+  32,69,115,116,101,114,110,101,0,129,
+  0,11,34,18,5,6,86,101,110,116,
+  111,108,97,0,129,0,2,66,42,5,
+  0,83,101,114,114,97,110,100,97,32,
+  71,97,114,97,103,101,0,129,0,12,
+  47,16,5,1,83,116,101,114,101,111,
+  0 };
   
-// this structure defines all the variables of your control interface 
+// Variabili che arrivano via bluetooth dall'interfaccia RemoteXY
 struct {
 
     // input variable
-  uint8_t luci_interne; // =1 if switch ON and =0 if OFF 
-  uint8_t luci_esterne; // =1 if switch ON and =0 if OFF 
-  uint8_t ventola; // =1 if switch ON and =0 if OFF 
-  uint8_t garage; // =0 if select position A, =1 if position B, =2 if position C, ... 
-  uint8_t generale; // =1 if button pressed, else =0 
+  uint8_t lights_in; // =1 if switch ON and =0 if OFF 
+  int8_t garage_door; // =0..100 slider position 
+  uint8_t lights_out; // =1 if switch ON and =0 if OFF 
+  uint8_t fan; // =1 if switch ON and =0 if OFF 
   uint8_t stereo; // =1 if switch ON and =0 if OFF 
+  uint8_t general; // =1 if button pressed, else =0 
 
     // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0 
@@ -66,14 +78,58 @@ struct {
 //           END RemoteXY include          //
 /////////////////////////////////////////////
 
+#define PIN_LIGHTS_IN 4
+#define PIN_LIGHTS_OUT 5
+#define PIN_FAN 6
+#define PIN_STEREO 7
+
+#define PIN_TEMPERATURE_SENSOR 2
+
+//LCD per mostrare temperatura e messaggi
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
+//Sensore temperatura
+DHT sensore_temperatura(PIN_TEMPERATURE_SENSOR,DHT11);
+
+
+/////////////////////////////////////////////
+//           FUNZIONI DI UTILITA           //
+/////////////////////////////////////////////
+
+
+// STAMPA MESSAGGIO MESSAGGIO VISUALIZZATO SU LCD
+// Temperatura e Umidita passati da fuori
+void aggiorna_lcd(float temperatura, float umidita)  
+{
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.setCursor(6,0);
+  lcd.print(temperatura);
+  lcd.setCursor(12,0);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Umid:");
+  lcd.setCursor(6,1);
+  lcd.print(umidita);
+  lcd.setCursor(12,1);
+  lcd.print("%");
+}
+
 
 
 void setup() 
 {
   RemoteXY_Init (); 
   
+  pinMode (PIN_LIGHTS_IN, OUTPUT);
+  pinMode (PIN_LIGHTS_OUT, OUTPUT);
+  pinMode (PIN_FAN, OUTPUT);
+  pinMode (PIN_STEREO, OUTPUT);
   
-  // TODO you setup code
+  // Inizializzazione display LCD e SENSORE TEMPERATURA
+  lcd.init();
+  lcd.backlight();
+  sensore_temperatura.begin();
   
 }
 
@@ -81,6 +137,10 @@ void loop()
 { 
   RemoteXY_Handler ();
   
+  digitalWrite(PIN_LIGHTS_IN, (RemoteXY.lights_in==0)?LOW:HIGH);
+  digitalWrite(PIN_LIGHTS_OUT, (RemoteXY.lights_out==0)?LOW:HIGH);
+  digitalWrite(PIN_FAN, (RemoteXY.fan==0)?LOW:HIGH);
+  digitalWrite(PIN_STEREO, (RemoteXY.stereo==0)?LOW:HIGH);
   
   // TODO you loop code
   // use the RemoteXY structure for data transfer
